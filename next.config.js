5afe4bbdef9ce841c819faafd6d0b289781260d8
Martin <MWFIAE@users.noreply.github.com>
2021-10-17 18:19:07 +0000

    Fixed webpack configuration to include node core modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve = {
+        alias: config.resolve.alias,
+        fallback: {
+          fs: require.resolve("browserify-fs"),
+          crypto: require.resolve("crypto-browserify"),
+          stream: require.resolve("stream-browserify"),
+          path: require.resolve("path-browserify"),
+        }
+      }
+    }
+
+    return config
+  }
 }
